a1634fd2f4c98a6af5213c8edba5bef885c45922
Shams Achmadi <sachmadi1@sheffield.ac.uk>
2024-12-09 11:03:46 +0000

    tweaking slightly

--- poker.hs
+++ poker.hs
@@ -1,14 +1,15 @@
-import Data.List (sort, group, sortBy, nub, subsequences)
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Avoid reverse" #-}
+import Data.List (sort, sortBy, group, nub, subsequences)
 import Data.Ord (comparing)
-import Data.Function (on)
 import System.Random
 
 -- Defining the datatypes for Deck
-data Suit = Hearts
-    | Diamonds
-    | Spades
+data Suit = Hearts 
+    | Diamonds 
+    | Spades 
     | Clubs
-    deriving (Eq, Ord, Show)
+    deriving (Show, Eq, Ord, Enum, Bounded)
 
 data Rank = Two
     | Three
@@ -29,10 +30,17 @@ type Card = (Suit, Rank)
 
 type Deck = [Card]
 
+-- Extract ranks and suits from a hand
+ranks :: [Card] -> [Rank]
+ranks hand = [rank | (_, rank) <- hand]
+
+suits :: [Card] -> [Suit]
+suits hand = [suit | (suit, _) <- hand]
+
 -- Defining the Player data type
 data Strategy = RandomStrategy
-              | PlaceholderStrategy
-              deriving (Show)
+    | PlaceholderStrategy
+    deriving (Show)
 
 data Player = Player {
     playerName :: String,
@@ -55,9 +63,7 @@ data GameState = GameState {
 } deriving (Show)
 
 buildDeck :: Deck
-buildDeck = [(suit, rank) | suit <- suitList, rank <- [Two .. King]]
-  where
-    suitList = [Hearts, Diamonds, Spades, Clubs]
+buildDeck = [(suit, rank) | suit <- [Hearts .. Clubs], rank <- [Two .. Ace]]
 
 shuffleDeck :: Deck -> IO Deck
 shuffleDeck [] = return []
@@ -70,8 +76,8 @@ shuffleDeck xs = do
 
 -- Functions to deal hole and community cards
 data DealType = DealHoleCards
-              | DealCommunityCards Int -- Number of cards to be dealt
-              deriving (Show)
+    | DealCommunityCards Int -- Number of cards to be dealt
+    deriving (Show)
 
 dealCards :: DealType -> GameState -> GameState
 dealCards dealType gameState =
@@ -105,23 +111,16 @@ dealToPlayers (player:restPlayers) deck =
             
 data HandRank -- Evaluating the rank of a hand
     = HighCard [Rank] -- The ranks in descending order
-    | OnePair Rank [Rank] -- The rank pair and the kickers e.g. OnePair King [Ten, Six, Three]
-    | TwoPair Rank Rank Rank -- The ranks of both pairs and the kicker e.g. TwoPair Queen Eight Two
-    | ThreeOfAKind Rank [Rank] -- The rank of the three of a king and the kicker e.g. ThreeOfAKind Jack [Nine, Five]
+    | OnePair Rank [Card] -- The rank pair and the kickers e.g. OnePair King [Ten, Six, Three]
+    | TwoPair Rank Rank -- The ranks of both pairs e.g. TwoPair Queen Eight
+    | ThreeOfAKind Rank -- The rank of the three of a kind e.g. ThreeOfAKind Jack
     | Straight Rank -- The highest card from the straight hand e.g. Straight Nine
     | Flush [Rank] -- The ranks in decending order of the flush e.g. Flush [King, Ten, Seven, Four, Two]
     | FullHouse Rank Rank -- The ranks of the three of a kind and pair e.g. FullHouse Three Eight
-    | FourOfAKind Rank Rank -- The ranks of the four of a kind and kicker e.g. FourOfAKind Five King
+    | FourOfAKind Rank -- The rank of the four of a kind e.g. FourOfAKind Five
     | StraightFlush Rank -- The highest card from the straight flush e.g. StraightFlush Eight
     | RoyalFlush
     deriving (Eq, Show, Ord)
-
--- Extract ranks and suits from a hand
-ranks :: [Card] -> [Rank]
-ranks hand = [rank | (_, rank) <- hand]
-
-suits :: [Card] -> [Suit]
-suits hand = [suit | (suit, _) <- hand]
 
 -- Convert Rank to numeric value for straights
 rankToValue :: Rank -> Int
@@ -156,7 +155,6 @@ valueToRank v = case v of
     12 -> Queen
     13 -> King
     14 -> Ace
-    _  -> error "Not a rank"
 
 -- Count occurrences of each Rank, used for checking for pairs
 rankCounts :: [Card] -> [(Rank, Int)]
@@ -166,62 +164,51 @@ rankCounts hand = map (\rGroup -> (head rGroup, length rGroup)) . group . sort $
 suitCounts :: [Card] -> [(Suit, Int)]
 suitCounts hand = map (\sGroup -> (head sGroup, length sGroup)) . group . sort $ suits hand
 
--- For evaluating the hands 
-
 highCard :: [Card] -> HandRank
 highCard hand = HighCard $ reverse . sort $ ranks hand
 
+-- Helper function used to find pairs, triplets and quads
+helperPair :: [Card] -> Int -> ([Rank], [Card])
+helperPair hand a =
+    let counts = rankCounts hand
+        matched = [r | (r, c) <- counts, c == a]
+        sorted = reverse $ sortBy (comparing snd) hand
+        remaining = filter (\(_, r) -> r `notElem` matched) sorted
+    in (matched, remaining)
+
 isOnePair :: [Card] -> Maybe HandRank
 isOnePair hand =
-    let counts = rankCounts hand
-        pairs = [rank | (rank, count) <- counts, count == 2] -- Check for pair
-        remaining = reverse . sort $ [rank | (rank, count) <- counts, count /= 2] -- Sort kickers
+    let (pairs, remaining) = helperPair hand 2
     in case pairs of
-        (p:_) -> Just (OnePair p remaining) -- If there is a pair, return OnePair
-        _     -> Nothing -- Else return Nothing
+        (p:_) -> Just (OnePair p remaining)
+        _     -> Nothing
 
 isTwoPair :: [Card] -> Maybe HandRank
 isTwoPair hand =
-    let counts = rankCounts hand
-        pairs = reverse . sort $ [rank | (rank, count) <- counts, count == 2] -- Check for two pairs
-        remaining = reverse . sort $ [rank | (rank, count) <- counts, count == 1] -- Sort kickers
+    let (pairs, _) = helperPair hand 2
     in case pairs of
-        (p1:p2:_) ->
-            let kicker = if not (null remaining) then head remaining else Two
-            in Just (TwoPair p1 p2 kicker) -- If there is two pairs, return TwoPair
-        _ -> Nothing -- If there are no pairs, return Nothing
+        (p1:p2:_) -> Just (TwoPair p1 p2)
+        _ -> Nothing
 
---Same as isTwoPair, except for three of a kind
 isThreeOfAKind :: [Card] -> Maybe HandRank
 isThreeOfAKind hand =
-    let counts = rankCounts hand
-        triplets = [rank | (rank, count) <- counts, count == 3]
-        remaining = reverse . sort $ [rank | (rank, count) <- counts, count /= 3]
-    in case triplets of
-        (r:_) -> Just (ThreeOfAKind r remaining)
+    let (trips, _) = helperPair hand 3
+    in case trips of
+        (t:_) -> Just (ThreeOfAKind t)
         _     -> Nothing
 
 isStraight :: [Card] -> Maybe HandRank
 isStraight hand =
-    case straightValue hand of
-        Just highRank -> Just (Straight highRank)
-        Nothing -> Nothing
-
--- Helper function to determine straights
-straightValue :: [Card] -> Maybe Rank
-straightValue hand =
-    let vs = map rankToValue (ranks hand)
-        vsWithAceLow = if Ace `elem` ranks hand then vs ++ [1] else vs -- If there is an Ace, add 1 to the list too
+    let cardRanks = ranks hand
+        vs = map rankToValue cardRanks
+        vsWithAceLow = if Ace `elem` cardRanks then vs ++ [1] else vs -- Add 1 to the list for a low ace in the hand
         uniqueVs = reverse . sort . nub $ vsWithAceLow
-        sequences = [take 5 (drop n uniqueVs) | n <- [0..(length uniqueVs - 5)]]
-        validSequences = [seq | seq <- sequences, isConsecutive seq] -- If there are 5 consecutive numbers
+        sequences = [take 5 (drop n uniqueVs) | n <- [0..(length uniqueVs - 5)]] -- We generate every sequence of cards we can, where it is consecutive
+        isConsecutive xs = and (zipWith (\a b -> a - b == 1) xs (tail xs)) -- Check if they are consecutive (straights)
+        validSequences = [seq | seq <- sequences, isConsecutive seq]
     in case validSequences of
-        (seq:_) -> Just (valueToRank (head seq)) -- Return the highest card from the hand
-        _       -> Nothing -- Else, return Nothing
-
--- Check if a list of integers is consecutive
-isConsecutive :: [Int] -> Bool
-isConsecutive xs = and $ zipWith (\a b -> a - b == 1) xs (tail xs)
+        (seq:_) -> Just (Straight (valueToRank (head seq)))
+        _       -> Nothing
 
 isFlush :: [Card] -> Maybe HandRank
 isFlush hand =
@@ -236,54 +223,37 @@ isFlush hand =
 
 isFullHouse :: [Card] -> Maybe HandRank
 isFullHouse hand =
-    let counts = rankCounts hand
-        threes = [rank | (rank, count) <- counts, count == 3] -- Look for any three of a kinds
-        pairs  = [rank | (rank, count) <- counts, count == 2] -- Look for pairs
-    in case (threes, pairs) of
-        ((t:_), (p:_)) -> Just (FullHouse t p) -- If it's found, return FullHouse
-        ((t:_), _)     -> -- If not pairs are found:
-            -- Check for another three of a kind to act as a pair
-            let remainingThrees = [rank | (rank, count) <- counts, count == 3, rank /= t]
-            in case remainingThrees of
-                (p:_) -> Just (FullHouse t p)
-                _     -> Nothing
-        _ -> Nothing -- if no full house, return Nothing
+    case isOnePair hand of
+        Nothing -> Nothing
+        Just (OnePair a xs) ->
+            case isThreeOfAKind xs of
+                Nothing -> Nothing
+                Just (ThreeOfAKind b) -> Just (FullHouse a b)
 
--- Same as isThreeOfAKind, except checking for 4 counts
 isFourOfAKind :: [Card] -> Maybe HandRank
 isFourOfAKind hand =
-    let counts = rankCounts hand
-        quads = [rank | (rank, count) <- counts, count == 4]
-        remaining = [rank | (rank, count) <- counts, count /= 4]
+    let (quads, _) = helperPair hand 4
     in case quads of
-        (r:_) -> Just (FourOfAKind r (maximum remaining))
+        (q:_) -> Just (FourOfAKind q)
         _     -> Nothing
 
 isStraightFlush :: [Card] -> Maybe HandRank
 isStraightFlush hand =
     case isFlush hand of
-        Just (Flush sortedRanks) ->
-            -- Using isFlush, if there is a flush then check if there is a straight too
-            let counts = suitCounts hand
-                flushSuits = [suit | (suit, count) <- counts, count >= 5]
-            in case flushSuits of
-                (s:_) ->
-                    -- Now that we have the flush suit, extract all cards of that suit
-                    let flushCards = filter (\(su, _) -> su == s) hand
-                    in case straightValue flushCards of -- We do not need to sort, since isFlush already has
-                        Just highRank -> Just (StraightFlush highRank)
-                        Nothing -> Nothing
-                _ -> Nothing -- If there is no straight, return Nothing
-        Nothing -> Nothing -- If there was no flush to begin with, return Nothing
+        Nothing -> Nothing
+        Just (Flush _) ->
+            case isStraight hand of
+                Nothing -> Nothing
+                Just (Straight highRank) -> Just (StraightFlush highRank)
 
 isRoyalFlush :: [Card] -> Maybe HandRank
 isRoyalFlush hand =
     case isStraightFlush hand of
+        Nothing -> Nothing
         Just (StraightFlush rank) ->
             if rank == Ace then Just RoyalFlush else Nothing -- Check if its a straight flush, 
                                                              -- if it's highest card is an Ace, 
                                                              -- then it is a royal flush
-        _ -> Nothing
 
 -- Finally, the evaluateHand function, goes through each in order of strength
 evaluateHand :: [Card] -> HandRank
@@ -325,4 +295,28 @@ determineWinner playerHandRanks
         let bestRank = maximum (map snd playerHandRanks)
             winners = [pName | (pName, rank) <- playerHandRanks, rank == bestRank]
         in winners
+
+main :: IO ()
+main = do
+    -- Step 1: Initialize the game state
+    let initialPlayers = [
+            Player "Alice" [] 1000 False RandomStrategy,
+            Player "Bob" [] 1000 False RandomStrategy
+            ]
+    let initialDeck = buildDeck
+    let initialGameState = GameState {
+            players = initialPlayers,
+            deck = initialDeck,
+            communityCards = [],
+            pot = 0,
+            bets = [],
+            dealerPosition = 0,
+            smallBlindPosition = 1,
+            bigBlindPosition = 2
+        }
+
+    let gameStateAfterHoleCards = dealCards DealHoleCards initialGameState
+    putStrLn "After dealing hole cards:"
+    putStrLn $ "Players' hands: " ++ show (map hand (players gameStateAfterHoleCards))
+    putStrLn $ "Deck size: " ++ show (length (deck gameStateAfterHoleCards))
 
